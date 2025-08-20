def118b07174354817ceabd56bb8e9b3e42f3d2c
Ling Zhang <vbzl1208@126.com>
2025-08-21 01:45:08 +0800

    feat: set & change progress

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import VoicemailIcon from '@mui/icons-material/Voicemail';
-import {Grid} from "@mui/material";
+import {Button, Grid} from "@mui/material";
 import EnglishPlayer from "./components/EnglishPlayer.jsx";
+import RadioIcon from "@mui/icons-material/Radio";
+import {useNavigate} from "react-router";
 
 function App() {
-
+    const navigate = useNavigate();
     return (
         <Grid container spacing={2}>
             <Grid size={4}>
-                <EnglishPlayer/>
+                <Button onClick={() => {
+                    navigate('/english_study')
+                }}>
+                    <RadioIcon/>
+                    <p>英语学习</p>
+                </Button>
             </Grid>
         </Grid>
     )
--- src/components/EnglishPlayer.jsx
+++ src/components/EnglishPlayer.jsx
@@ -1,26 +1,89 @@
-import RadioIcon from '@mui/icons-material/Radio';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import StopIcon from '@mui/icons-material/Stop';
 import {axiosInstance} from "../utils/network.jsx";
 import {useEffect, useState} from "react";
-import {Button} from "@mui/material";
+import {Alert, Box, Button, Container, Grid, Snackbar} from "@mui/material";
 
 export default function EnglishPlayer() {
-    const [isPlaying, setIsPlaying] = useState(false)
+    const [status, setStatus] = useState(false)
+    const [alertConfig, setAlertConfig] = useState({show: false})
+    console.log(status)
     useEffect(() => {
-        axiosInstance.get("/api/english_player/is_playing").then((response) => {
-            setIsPlaying(response.is_playing)
+        axiosInstance.get("/api/english_player/get_status").then((response) => {
+            setStatus(response)
         })
-    });
+    }, []);
     const clickHandler = () => {
-        const url = isPlaying ? "/api/english_player/stop" : "/api/english_player/start"
+        const url = status.is_playing ? "/api/english_player/stop" : "/api/english_player/start"
         axiosInstance.post(url).then(() => {
-            setIsPlaying(!isPlaying)
+            location.reload()
         })
     }
+    if (!status) {
+        return <></>
+    }
+    const showAlert = (severity, message) => {
+        setAlertConfig({
+            show: true,
+            severity: severity,
+            message: message
+        })
+        setTimeout(() => {
+            setAlertConfig({
+                show: false,
+                severity: "info",
+                message: ""
+            })
+        }, 3000)
+    }
     return (
-        <Button onClick={clickHandler}>
-            <RadioIcon/>
-            <p>{isPlaying ? "停止播放英语" : "播放英语"}</p>
-        </Button>
+        <Box>
+            <Container>
+                <Button onClick={clickHandler}>
+                    {status.is_playing ? <StopIcon/> : <PlayArrowIcon/>}
+                    <p>{status.is_playing ? "停止播放英语" : "播放英语"}</p>
+                </Button>
+            </Container>
+            <Container>
+                学习进度: {<input type="number"
+                                  disabled={status.is_playing}
+                                  value={status.study_progress} min={status.cursor + 1}
+                                  max={status.audio_count}
+                                  onChange={(e) => {
+                                      setStatus({...status, study_progress: parseInt(e.target.value)})
+                                  }}/>}
+            </Container>
+            <Container>
+                当前播放: {<input type="number" disabled={status.is_playing} value={status.cursor} min={0}
+                                  max={status.study_progress - 1}
+                                  onChange={(e) => {
+                                      setStatus({...status, cursor: parseInt(e.target.value)})
+                                  }}/>}
+            </Container>
+            {
+                !status.is_playing && <Container>
+                    <Button onClick={() => {
+                        axiosInstance.post('/api/english_player/set_progress', {
+                            study_progress: status.study_progress,
+                            cursor: status.cursor,
+                        }).then(() => {
+                            showAlert("success", "修改成功")
+                        }).catch(() => {
+                            showAlert("error", "修改失败")
+                        })
+                    }}>保存配置</Button>
+                </Container>
+            }
+            {status.is_playing && <Container>
+                当前播放文件名：{status.current_file}
+            </Container>}
+            {
+                alertConfig.show &&
+                <Alert variant="filled" severity={alertConfig.severity}>
+                    {alertConfig.message}
+                </Alert>
+            }
 
+        </Box>
     )
 }
