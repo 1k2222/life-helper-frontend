9509a25978fd4f9e8283142182b93191e5733165
Ling Zhang <vbzl1208@126.com>
2025-09-13 03:40:29 +0800

    fix: update voice after regeneration.

--- src/components/CantonesePronouncer.jsx
+++ src/components/CantonesePronouncer.jsx
@@ -1,11 +1,12 @@
 import {Box, Button, CircularProgress, Grid, TextField} from "@mui/material";
-import {useState} from "react";
+import {useRef, useState} from "react";
 import axios from "axios";
 
 export default function CantonesePronouncer() {
     const [status, setStatus] = useState(0)
     const [text, setText] = useState("")
     const [audioContent, setAudioContent] = useState(null)
+    const audioRef = useRef()
     const generate = () => {
         setStatus(1)
         axios.post('/api/pronounce_cantonese', {
@@ -15,6 +16,9 @@ export default function CantonesePronouncer() {
         }).then((resp) => {
             setStatus(0)
             setAudioContent(resp.data)
+            if (audioRef.current) {
+                audioRef.current.load();
+            }
         }).catch((error) => {
             setStatus(2)
             console.log("Pronounce Cantonese Failed, error: ", error)
@@ -46,7 +50,7 @@ export default function CantonesePronouncer() {
             return null
         }
         return (
-            <audio controls>
+            <audio controls ref={audioRef}>
                 <source src={URL.createObjectURL(audioContent)} type="audio/aac"/>
             </audio>
         )
